3047625bcdba4d09c72c0fcdd7c6d64e83c7c5e7
Saif <saifali962853@gmail.com>
2022-10-14 23:41:57 +0530

    Rename Cahpter1 and 2

---
+++ Chapter6(Conditionals_expressions)/06_practice_set02.py
@@ -0,0 +1,1 @@
+# Write a  program to find out whether a student is pass or fail, if it requires total 40% and at least 33% in each subject to pass assume 3 subject and take marks as an inout from the user 
